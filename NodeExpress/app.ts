52929a8fd56a1618d5c58bbf0bb46c4e70d330ad
Tom <thomastffischer@gmail.com>
2016-03-08 19:42:44 +0100

    removed stylus and jade

--- NodeExpress/app.ts
+++ NodeExpress/app.ts
@@ -7,8 +7,6 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 app.use(express.static("../AngularClient"));
 app.use(express.favicon());
 app.use(express.logger('dev'));
@@ -17,8 +15,6 @@ app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
 
-import stylus = require('stylus');
-app.use(stylus.middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
